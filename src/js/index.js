384fc094aaa4d5265c6e308369f74705e8a9b549
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-02-23 12:31:32 +0530

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,6 @@
-/* This is a function that is listening for the DOM to be ready. */
-document.addEventListener("DOMContentLoaded", () => {
-  initApp();
+const allBlogs = document.querySelector("#allBlogs");
+
+allBlogs.addEventListener("click", function (e) {
+  const toggleItems = document.querySelector("#toggleItems");
+  toggleItems.classList.toggle("toggle-items");
 });
-
-const initApp = () => {};
-
-// querySelectorAll function
-function qsAll(selector, parent = document) {
-  return [...parent.querySelectorAll(selector)];
-}
-
-// querySelector function
-function qs(selector, parent = document) {
-  return parent.querySelector(selector);
-}
